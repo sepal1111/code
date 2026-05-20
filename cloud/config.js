33f0d2e5c5dcb2acf868a89dca44ab8e20a10b44
sepal <sepal1111@gmail.com>
2026-05-20 11:02:49 +0800

    V4.2

--- cloud/config.js
+++
@@ -1,6 +0,0 @@
-// config.js - 雲端 Google Sheets 串接設定檔
-window.CONFIG = {
-    // 💡 請在此貼上您的 Google Apps Script 網頁應用程式 (Web App) 網址
-    // 例如: "https://script.google.com/macros/s/AKfycb...你的ID.../exec"
-    GOOGLE_SHEETS_API_URL: "https://script.google.com/macros/s/AKfycbxJlVR79liNsnFgEV1gNuFkxJSM31JhJnAHGnsbGog3MNnNh43fqCw__AnT_HS9avLk/exec"
-};
